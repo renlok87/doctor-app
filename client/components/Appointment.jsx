4d7bc9c64a39b3aece64e1ef5c4e93133e78aae8
renlok87 <renlok87@gmail.com>
2020-03-24 11:04:51 +0300

    initial

---
+++ client/components/Appointment.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { View } from 'react-native';
+import styled from 'styled-components/native';
+import GrayText from "./GrayText";
+import Badge from "./Badge";
+
+const Appointment = ({ navigate, item }) => {
+    const { user, diagnosis, active, time } = item;
+    return (
+        <GroupItem onPress={navigate.bind(this, 'Patient', item)}>
+            <Avatar
+                source={{
+                    uri: user.avatar
+                }}
+            />
+            <View style={{ flex: 1 }}>
+                <FullName>{user.fullname}</FullName>
+                <GrayText>{diagnosis}</GrayText>
+            </View>
+            <Badge active={active}>{time}</Badge>
+        </GroupItem>
+    );
+};
+
+Appointment.defaultProps = {
+    groupTitle: 'Untitled',
+    items: []
+};
+
+const FullName = styled.Text`
+  font-weight: 600;
+  font-size: 16px;
+`;
+
+const Avatar = styled.Image`
+  border-radius: 50px;
+  width: 40px;
+  height: 40px;
+  margin-right: 15px;
+`;
+
+const GroupItem = styled.TouchableOpacity`
+  align-items: center;
+  flex-direction: row;
+  padding: 20px;
+  border-bottom-width: 1px;
+  border-bottom-color: #f3f3f3;
+`;
+
+export default Appointment;
